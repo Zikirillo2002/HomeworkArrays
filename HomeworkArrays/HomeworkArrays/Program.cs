fca20c7ec1ba632f1af0352ebe9fc48d5fed3b69
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-06 17:32:24 +0500

    Added Task number 7

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,66 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n;
+            int indexmins = 0;
+            int mins = 10;
+            bool isnumber;
+
+            Console.Write("Enter the arry length n = ");
+            isnumber = int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine();
+
+            while (!isnumber || n <= 0)
+            {
+                Console.WriteLine($"An error occurred in the system !!! \n Please enter another number !!! ");
+                Console.WriteLine();
+                Console.Write("Enter the arry length n = ");
+                isnumber = int.TryParse(Console.ReadLine(), out n);
+                Console.WriteLine();
+            }
+
+
+            int[] arry = new int[n];
+            int[] arrymin = new int[n];
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                arry[i] = random.Next(0, 10);
+                Console.WriteLine($" arry[{i}] = {arry[i]}");
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mins > arry[j])
+                    {
+                        mins = arry[j];
+                        arrymin[i] = mins;
+                        indexmins = j;
+                    }
+                }
+                arry[indexmins] = 10;
+                mins = 10;
+            }
+
+            Console.WriteLine("  Placement in ascending order  ");
+            Console.WriteLine();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($" {i + 1}). {arrymin[i]}");
+
+            }
+
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+            Main(args);
         }
     }
 }
