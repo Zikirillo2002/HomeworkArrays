4ba2e8727c11e418ff03bd6bd7bbb74001c9c1dc
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-06 17:06:01 +0500

    Added Task number 1

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,36 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int OddNumbers = 1;
+            bool isnumber;
+
+            Console.Write(" Enter your number : n = ");
+            isnumber = int.TryParse(Console.ReadLine(), out int number);
+            Console.WriteLine();
+
+            while (!isnumber || number < 0)
+            {
+                Console.WriteLine($" An error occurred in the system !!! \n Please enter another number !!! ");
+                Console.WriteLine();
+                Console.Write(" Enter your number : n = ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+                Console.WriteLine();
+            }
+
+            int[] array = new int[number];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = OddNumbers;
+                OddNumbers += 2;
+                Console.WriteLine($" {i + 1}). array[{i}] = {array[i]}");
+                
+            }
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+            Main(args);
+
         }
     }
 }
