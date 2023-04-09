e58735f3476e5aecf486a604d968b851ad12f941
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 14:47:09 +0500

    Added Task String 1

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,35 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetNumber();
+
         }
+
+        public static void GetNumber()
+        {
+            Console.Write(" Enter your age : ");
+            bool isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isnumber)
+            {
+                Console.WriteLine(" An error occurred in the system !!! \n Please enter another number. ");
+                Console.Write(" Enter your age : ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            Biography(number);
+
+
+        }
+
+        public static void Biography(int age)
+        {
+
+            Console.WriteLine($" I'm {age} years old.");
+        }
+
+
+
     }
 }
+
