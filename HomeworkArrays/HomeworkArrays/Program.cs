fdd0da0448a3b1cb59601b544f80888d989c1655
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 18:58:11 +0500

    Added Task String 4

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,50 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FindProbel();
+        }
+
+        public static void FindProbel()
+        {
+            string text = GetText();
+            string str = "";
+            string newText = "";
+
+            for(int i = text.Length - 1; i > 0; i--)
+            {
+                str += text[i];
+            }
+
+            int indexProbelFirst = text.IndexOf(" ");
+            int indexProbelEnd = str.IndexOf(" ");
+
+            if (indexProbelEnd > 1)
+            {
+
+                for (int i = indexProbelFirst; i < (text.Length - indexProbelEnd); i++)
+                {
+                    newText += text[i];
+                }
+
+                Console.WriteLine(newText);
+
+            }
+
+            else
+            {
+                Console.WriteLine("_");
+            }
+
+            
+        }
+
+        public static string GetText()
+        {
+
+            Console.Write(" Enter your text : ");
+
+            return Console.ReadLine();
+
         }
     }
 }
