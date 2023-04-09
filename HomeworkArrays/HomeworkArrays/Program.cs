f0e4db1ad0826d57c6c797f3b6d5aa7ce236e059
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 18:19:12 +0500

    Added Task String 3

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,28 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ReverseText();
+        }
+
+        public static void ReverseText()
+        {
+            string text = GetText();
+            string str = "";
+
+            for(int i = text.Length - 1; i >= 0 ;i--)
+            {
+                str += text[i];
+            }
+
+            Console.WriteLine(str);
+        }
+
+        public static string GetText()
+        {
+            Console.Write(" Enter your text : ");
+
+            return Console.ReadLine();
+
         }
     }
 }
