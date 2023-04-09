6c65c2ccb134b6792a4d8f4361fb788abac3f66a
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 18:04:32 +0500

    Added Task String 2

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,26 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ToLowerWord();
+        }
+
+        public static void ToLowerWord()
+        {
+            string word = GetText();
+
+            Console.WriteLine(word.ToLower());
+        }
+
+        public static string GetText()
+        {
+
+            Console.Write(" Enter your word : ");
+            string text = Console.ReadLine();
+
+            return text;
+
         }
     }
+
+    
 }
