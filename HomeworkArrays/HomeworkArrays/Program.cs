b07f37d00ebcbef19d9e210d55852f927a09fef3
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 13:04:38 +0500

    Added Task Method 1

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,36 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SignFind();
+          
+        }
+
+        public static void SignFind()
+        {
+            int number = GetNumber();
+
+            if (number < 0)
+                Console.WriteLine($" {number} is negative number ");
+            else
+                Console.WriteLine($" {number} is positive number ");
+        }
+
+        public static int GetNumber()
+        {
+            bool isnumber;
+
+            Console.Write(" Enter your number : ");
+            isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isnumber)
+            {
+                Console.WriteLine(" An error occured in the system \n Please enter another number.");
+                Console.Write(" Enter your number : ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
         }
     }
 }
+
