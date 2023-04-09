ebf28a528605041e8ee70330c0abfc2817d0b02a
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 13:40:06 +0500

    Added Task Method 3

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,39 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+           HowManyRooms();   
+        }
+
+        public static void HowManyRooms()
+        {
+            int number = GetNumber();
+            int firstNumber = number;
+            int rooms = 0;
+
+            while (number > 0)
+            {
+                number /= 10;
+                rooms++;
+
+            }
+
+            Console.WriteLine($" {firstNumber} is number {rooms} rooms.");
+        }
+
+        public static int GetNumber()
+        {
+            Console.Write(" Enter your number : ");
+            bool isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isnumber || number <= 0)
+            {
+                Console.WriteLine(" An error occurred in the system !!! \n Please enter another number.");
+                Console.Write(" Enter your number : ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
         }
     }
 }
