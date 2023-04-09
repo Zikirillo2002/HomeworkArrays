238355285f3790bff9adbb809754a8d9095c96f9
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 13:21:01 +0500

    Added Task Method 2

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,31 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($" {FindCentury() + 1} centuries ");
+        }
+
+        public static int FindCentury()
+        {
+            int number = GetNumber();
+
+            int century = number / 100;
+
+            return century;
+        }
+
+        public static int GetNumber()
+        {
+            Console.Write(" Enter your number : ");
+            bool isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+            while(!isnumber || number <= 0)
+            {
+                Console.WriteLine(" An error occurred in the system !!! \n Please enter another number.");
+                Console.Write(" Enter your number : ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
         }
     }
 }
