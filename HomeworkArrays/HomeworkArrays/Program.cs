75a773c0aebba2aecd7ee720a6e89569ce9880e7
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-09 13:53:37 +0500

    Added Task Method 4

--- HomeworkArrays/HomeworkArrays/Program.cs
+++ HomeworkArrays/HomeworkArrays/Program.cs
@@ -4,7 +4,41 @@
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MinAndMax();
+        }
+
+        public static void MinAndMax()
+        {
+            Console.Write(" Enter your number x = ");
+            int x = GetNumber();
+            Console.Write(" Enter your number y = ");
+            int y = GetNumber();
+            int c = x + y;
+
+            if(x < y)
+            {
+                x = y;
+                y = c - x;
+            }
+
+            Console.WriteLine($" Max number : x = {x} \n Min number : y = {y}");
+           
+        }
+
+        public static int GetNumber()
+        {
+            bool isnumber;
+
+            isnumber = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isnumber)
+            {
+                Console.WriteLine(" An error occured in the system \n Please enter another number.");
+                Console.Write(" Enter your number : ");
+                isnumber = int.TryParse(Console.ReadLine(), out number);
+            }
+
+            return number;
         }
     }
 }
